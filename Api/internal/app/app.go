04762623a14cdd82cce1eae1139c196ea36ffa94
noH4uK90 <noh4uk90@gmail.com>
2024-02-06 15:50:36 +0300

    Add basic inits

--- Api/internal/app/app.go
+++ Api/internal/app/app.go
@@ -1,6 +1,11 @@
 package app
 
-import "context"
+import (
+	"Api/internal/config"
+	"context"
+	"github.com/go-chi/chi/v5"
+	"log"
+)
 
 type App struct {
 	serviceProvider *ServiceProvider
@@ -14,4 +19,27 @@ func NewApp(ctx *context.Context) (*App, error) {
 
 func (a *App) Run() error {
 	return nil
+}
+
+func (a *App) initDeps(ctx *context.Context) error {
+	return nil
+}
+
+func (a *App) initEnv(_ *context.Context) error {
+	err := config.Load(".env.local")
+	if err != nil {
+		log.Fatalf("Error loadl config: %s", err.Error())
+	}
+	return nil
+}
+
+func (a *App) initServiceProvider(_ *context.Context) error {
+	a.serviceProvider = newServiceProvider()
+	return nil
+}
+
+func initRouter(a *App) (*chi.Mux, error) {
+	r := chi.NewRouter()
+	//postgres := a.serviceProvider.Postgres()
+	return r, nil
 }
